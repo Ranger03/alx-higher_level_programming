640ad1417c8d2226b28fa1ec0db26ce99dfde0ba
Ranger03 <sakhilekgaphola.ss@gmail.com>
2023-06-14 10:55:27 -0700

    Task Done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for i in range(3):
-        for j in range(3):
+    for i in range(2):
+        for j in range(2):
             matrix[i][j] = (matrix[i][j])**2
